d76fd91256dbdb34f9801a1a541c10a5dc59ffb4
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2023-12-21 19:44:04 +0500

    Added new two problems

--- script.js
+++ script.js
@@ -15,14 +15,61 @@
 
 //! Sum factorial 
 
-let userNum = +prompt('Enter a number: ');
-let sumResult = 0;
+// let userNum = +prompt('Enter a number: ');
+// let sumResult = 0;
 
-if (!isNaN(userNum) && userNum > 0) {
-    for (i = 1; i <= userNum; i++) {
-        sumResult += i;
+// if (!isNaN(userNum) && userNum > 0) {
+//     for (i = 1; i <= userNum; i++) {
+//         sumResult += i;
+//     }
+//     alert(`The sum starting from 1 till your given number is: ${sumResult}`);
+// } else {
+//     alert('Wrong input(s)');
+// }
+
+
+//! Problem A
+
+function ageCalc(name, birthYear, presentYear) {
+    //? getting user inputs
+    name = prompt('Enter your name: ');
+    birthYear = +prompt('Enter your birth year: ');
+    presentYear = +prompt('Enter the present year: ');
+    let realAge = presentYear - birthYear;
+
+    //? logic part 
+    if (!isNaN(birthYear) && !isNaN(presentYear) && name != '') {
+        return alert(`1. Name: ${name}\n2. Birth Year: ${birthYear}\n3. Present Year: ${presentYear}\n${name}, your age is ${realAge}`);
+    } else {
+        alert('Error not data.');
     }
-    alert(`The sum starting from 1 till your given number is: ${sumResult}`);
-} else {
-    alert('Wrong input(s)');
-}
+};
+ageCalc();
+
+
+//! Problem B
+
+function randomProblems(userRandomNumber, userAnswers) {
+    //? getting some inputs 
+    userRandomNumber = +prompt('Enter the number of problems you want to solve: ');
+    // userAnswers = +prompt();
+    // doing random operators
+    const operators = ['+', '-', '*', '/'];
+    const randomOperators = operators[Math.floor(Math.random() * operators.length)];
+    // doing random numbers 
+    let num1 = Math.floor(Math.random() * 100) + 1;
+    let num2 = Math.floor(Math.random() * 100) + 1;
+
+    //? implementing some logic
+    if (!isNaN(userRandomNumber) && !isNaN(userAnswers)) {
+        for(i = 0; i <= userRandomNumber; i++) {
+            let problem = `${num1} ${randomOperators} ${num2} =`;
+            alert(problem += i);
+        };
+    } else if (operators == '/' && num2 == 0) {
+        return randomProblems();
+    } else {
+        alert('Error not data.');
+    }
+};
+randomProblems();
